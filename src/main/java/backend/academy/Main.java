d190f7c080be7d72e1caa90f43315d412d17d198
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-12-06 14:52:12 +0300

    feat(core): добавлен запуск генерации

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,22 @@
 package backend.academy;
 
+import backend.academy.format.ImageFormat;
+import backend.academy.utils.ImageUtils;
+import java.nio.file.Path;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
+
     public static void main(String[] args) {
+        var fractalImage = FractalFlameGenerator.generateRandom(false);
+
+        ImageUtils.saveFractalFlame(
+            fractalImage,
+            Path.of("output/fractal.png"),
+            ImageFormat.PNG
+        );
+
+        System.out.println("Фрактал успешно сохранен в output/fractal.png");
     }
 }
