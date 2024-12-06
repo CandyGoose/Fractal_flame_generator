a52beb1bfb7499b8776eed4fac2179d125d16949
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-12-06 11:28:12 +0300

    feat(models): добавлен класс FractalImage

---
+++ src/main/java/backend/academy/model/FractalImage.java
@@ -0,0 +1,24 @@
+package backend.academy.model;
+
+public record FractalImage(Pixel[][] data, int width, int height) {
+
+    public static FractalImage create(int width, int height) {
+        FractalImage fractalImage = new FractalImage(
+            new Pixel[height][width],
+            width,
+            height
+        );
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                fractalImage.data[i][j] = new Pixel(j, i, Color.getEmpty());
+            }
+        }
+        return fractalImage;
+    }
+
+    public boolean contains(int x, int y) {
+        return x >= 0 && x < width
+            && y >= 0 && y < height;
+    }
+}
